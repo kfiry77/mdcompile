bcd5c2e7b7c59a7b195227215da6b289177d730f
Kfir Ezekiel <kfir.ye@gmail.com>
2024-08-24 18:16:13 +0300

    adding git commit to docker script

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,3 +14,19 @@ for file in $(find . -name '*.src.md'); do
   python /app/process_codemd.py "$file" "$INPUT_OUTPUT_DIR"
 done
 
+
+# Set up git and push changes
+git config --local user.email "actions@github.com"
+git config --local user.name "GitHub Actions"
+echo "*.puml" > "$1/.gitignore"
+git add "$1/.gitignore"
+git add "$1"/*.svg
+git add *.md
+
+if git diff --quiet; then
+  echo "No changes to commit."
+else
+  git commit -m "Automated update of Markdown files"
+  git push
+fi
+
