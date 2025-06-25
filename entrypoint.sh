efd82e0080a285e3cf58329594c7b6ad9f7352b9
Kfir Yehezkel <kfir.yehezkel@intl.att.com>
2025-06-25 22:08:58 +0300

    changing to use the entry script instead of step in the yaml.  removing docker file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,15 @@ fi
 # Ensure the output directory exists
 mkdir -p "$INPUT_OUTPUT_DIR"
 
+# Activate the virtual environment
+if [ -f "/app/venv/bin/activate" ]; then
+  . /app/venv/bin/activate
+else
+  echo "Virtual environment not found at /app/venv. Exiting."
+  exit 1
+fi
+
 # Process all .code.md files in the repository
 for file in $(find . -name '*.src.md'); do
   python /app/process_codemd.py "$file" "$INPUT_OUTPUT_DIR"
 done
-
